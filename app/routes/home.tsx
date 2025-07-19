7fdda7a19cbeeea6f487cd924489d50502c7a8d7
Haritha Ganegoda <haritha.99ganegoda@gmail.com>
2025-07-19 21:58:39 +0530

    Resume Card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,22 +1,36 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "ResuMatch" },
-    { name: "description", content: "Matching talent to opportunity — intelligently" },
+    {
+      name: "description",
+      content: "Matching talent to opportunity — intelligently",
+    },
   ];
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>Track Your Applications & Resume Ratings</h1>
-        <h2>Review your submissions and get AI-powered feedback</h2>
-      </div>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and get AI-powered feedback</h2>
+        </div>
 
-    </section>
-  </main>;
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
